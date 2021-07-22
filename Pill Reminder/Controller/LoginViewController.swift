f8b63f875c357e424e9a13919ffa6d21c5c13ab2
vtodorov78 <vvtodorov12@gmail.com>
2021-07-23 00:04:38 +0300

    Initial Commit

---
+++ Pill Reminder/Controller/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pill Reminder
+//
+//  Created by Vladimir Todorov on 22.07.21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ Pill Reminder/Controller/RegistrationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationViewController.swift
+//  Pill Reminder
+//
+//  Created by Vladimir Todorov on 23.07.21.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
